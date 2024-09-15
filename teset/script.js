015d5d9759e97f99a85e44a21f7599a892bf3c62
qelsimrraci <qelsim2005@gmail.com>
2024-09-16 00:51:44 +0100

    Initial commit for portfolio website

--- teset/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuOverlay = document.getElementById('menuOverlay');
-    if (menuOverlay.style.display === 'block') {
-        menuOverlay.style.display = 'none';
-    } else {
-        menuOverlay.style.display = 'block';
-    }
-}
